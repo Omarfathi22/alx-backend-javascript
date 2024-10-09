ce5c0a79a056198c054ce31749b8de3a259e9e9b
Omarfathi22 <ommarf525@gmail.com>
2024-10-09 12:57:26 +0300

    4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,26 @@
+export default function updateStudentGradeByCity(
+	  students,
+	  city,
+	  newGrades,
+) {
+	  const filterStudents = students.filter(
+		      (student) => student.location === city,
+		    );
+
+	  const gradeStudents = filterStudents.map(
+		      (student) => {
+			            for (const gradeInfo of newGrades) {
+					            if (student.id === gradeInfo.studentId) {	
+							    student.grade = gradeInfo.grade; // eslint-disable-line no-param-reassign
+							            }
+					          }
+			            if (student.grade === undefined) {
+					            student.grade = 'N/A'; 
+					          }
+			            return student;
+			          },
+		    );
+
+	  return gradeStudents;
+}
+
